ddac12902359599265eebb5a931f7e9b23c6c529
potemin1999 <potemin1999@bk.ru>
2017-05-15 18:31:06 +0300

    added api.cpp

--- sources/db/api.h
+++ sources/db/api.h
@@ -24,36 +24,9 @@ int dbCloseEntry();
 int dbEnterInteractive();
 //interactive.cpp
 
-int dbInitDB(char* name,char* options){
-    db = new DB::IncreaseDB(name);
-    db->init(options);
-    hm = db->getHeadManager();
-    dm = db->getDataManager();
-    return 0;
-}
-
-int dbDestoyDB(){
-    return db->free();
-}
-
-int dbCreateEntry(entry* new_entry_p){
-    new_entry_p->id = db->create_entry();
-    return 0;
-}
-
-int dbOpenEntry(entry* entry_p){
-    return db->open_entry((Id*)&entry_p->id,0);
-}
-
-
-
 
 
 #ifdef NEW_NAMES
-
-
-
-
 
 /**db init*/
 typedef int (db_init)(char* name,char* options);
@@ -79,12 +52,11 @@ typedef int (db_close_entry)();
 typedef int (db_enter_interactive)();
 #define db_enter_interactive dbEnterInteractive
 
-
-
-
-
-
 #endif // NEW_NAMES
+
+
+
+
 
 
 #endif // API_H_INCLUDED
